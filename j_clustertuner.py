2899506f8b20777bea80a24a536b7b84e7a9b825
jakirk4 <jaimesgeemail@gmail.com>
2025-06-10 17:10:01 -0400

    updates

--- j_clustertuner.py
+++ j_clustertuner.py
@@ -1,41 +1,67 @@
 import j_process
-def kmode_tune(train_set, val_set, features,target):
-    results=[]
+
+from joblib import Parallel, delayed
+
+def kmode_tune(train_set, val_set, features, use_multiprocessing=True, cores=8):
+    results = []
+    der_var = ["ALL_CHRONIC", "ALL_CARDIAC", "ALL_PUL"]
+
     print("Starting Baseline...")
-    base_score = j_process.run_logistic_model(train_set,val_set,features,target)
+    base_score_all = j_process.run_logistic_model(train_set, val_set, features, der_var[0])
+    base_score_car = j_process.run_logistic_model(train_set, val_set, features, der_var[1])
+    base_score_pul = j_process.run_logistic_model(train_set, val_set, features, der_var[2])
     results.append({
         "Iteration": 0,
-        "Performance": base_score,
-        "Train Clusters" : [],
-        "Val Clusters" : []
+        "Performance": [base_score_all, base_score_car, base_score_pul],
+        "Train Clusters": [],
+        "Val Clusters": []
     })
-    i=2
-    print("Beginning Trials")
-    while i != 65:
-        print("Trial")
-        print(i)
-        print("Clustering training set...")
-        tdf_out, kmodename = j_process.run_kmodes_cluster(train_set,features,n_clusters=i,verbose=0)
-        print("Clustering valadation set...")
-        vdf_out, kmodename2 = j_process.run_kmodes_cluster(val_set,features,n_clusters=i,verbose=0)
+
+    def run_trial(i):
+        print(f"Starting trial {i}")
+        tdf_out, kmodename = j_process.run_kmodes_cluster(train_set, features, n_clusters=i, verbose=0)
+        vdf_out, kmodename2 = j_process.run_kmodes_cluster(val_set, features, n_clusters=i, verbose=0)
+
         tfeatures = features.copy() + [kmodename]
-        acc_score = j_process.run_logistic_model(tdf_out,vdf_out,tfeatures,target)
-        results.append({
+        all_acc_score = j_process.run_logistic_model(tdf_out, vdf_out, tfeatures, der_var[0])
+        car_acc_score = j_process.run_logistic_model(tdf_out, vdf_out, tfeatures, der_var[1])
+        pul_acc_score = j_process.run_logistic_model(tdf_out, vdf_out, tfeatures, der_var[2])
+
+        print(f"Finished trial {i}")
+        return {
             "Iteration": i,
-            "Performance": acc_score,
-            "Train Clusters": tdf_out[kmodename],
-            "Val Clusters": vdf_out[kmodename2]
-        })
-        i = i + 1
+            "Performance": [all_acc_score, car_acc_score, pul_acc_score],
+            "Train Clusters": tdf_out[kmodename].tolist(),
+            "Val Clusters": vdf_out[kmodename2].tolist()
+        }
+
+    print("Beginning Trials")
+    if use_multiprocessing:
+        print(f"Running in parallel with {cores} cores.")
+        parallel_results = Parallel(n_jobs=cores, backend="loky")(
+            delayed(run_trial)(i) for i in range(2, 65)
+        )
+        results.extend(parallel_results)
+    else:
+        print("Running sequentially.")
+        for i in range(2, 65):
+            results.append(run_trial(i))
 
     return results
-def tflow_tune(train_set, val_set, features,target):
+
+
+def tflow_tune(train_set, val_set, features):
     results=[]
+    der_var=["ALL_CHRONIC","ALL_CARDIAC","ALL_PUL"]
+
     print("Starting Baseline...")
-    base_score = j_process.run_logistic_model(train_set,val_set,features,target)
+    print("Starting Baseline...")
+    base_score_all = j_process.run_logistic_model(train_set,val_set,features,der_var[0])
+    base_score_car = j_process.run_logistic_model(train_set,val_set,features,der_var[1])
+    base_score_pul = j_process.run_logistic_model(train_set,val_set,features,der_var[2])
     results.append({
         "Iteration": 0,
-        "Performance": base_score,
+        "Performance": [base_score_all,base_score_car,base_score_pul],
         "Train Clusters" : [],
         "Val Clusters" : []
     })
@@ -49,12 +75,15 @@ def tflow_tune(train_set, val_set, features,target):
         print("Clustering valadation set...")
         vdf_out, kmodename2 = j_process.run_tf_clustering(val_set,features,n_clusters=i,verbose=0)
         tfeatures = features.copy() + [kmodename]
-        acc_score = j_process.run_logistic_model(tdf_out,vdf_out,tfeatures,target)
+        all_acc_score = j_process.run_logistic_model(tdf_out,vdf_out,tfeatures,der_var[0])
+        car_acc_score = j_process.run_logistic_model(tdf_out,vdf_out,tfeatures,der_var[1])
+        pul_acc_score = j_process.run_logistic_model(tdf_out,vdf_out,tfeatures,der_var[2])
+        
         results.append({
             "Iteration": i,
-            "Performance": acc_score,
-            "Train Clusters": tdf_out[kmodename],
-            "Val Clusters": vdf_out[kmodename2]
+            "Performance": [all_acc_score, car_acc_score,pul_acc_score],
+            "Train Clusters": tdf_out[kmodename].tolist(),
+            "Val Clusters": vdf_out[kmodename2].tolist()
         })
         i = i + 1
 
